6ee9cda15131f64dfd2af7a60ebd7c39d6bdc636
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2025-02-01 14:45:49 +0100

    feat: can chose between different algos in get_ai_move

--- src/ai_move.c
+++ src/ai_move.c
@@ -4,10 +4,19 @@
 #include "connect4.h"
 #include <stdlib.h>
 
-int get_ai_move(t_game *game) {
-    (void)game;
-    // Implement your AI here
-    return rand() % game->board.cols;
+int get_ai_move(t_game *game, t_ai_algo ai_algo) {
+    if (ai_algo == AI_RANDOM) {
+        return rand() % game->board.cols;
+    } else if (ai_algo == AI_GREEDY) {
+        return 0; // Implement your AI here
+    } else if (ai_algo == AI_MINIMAX) {
+        return 0; // Implement your AI here
+    } else if (ai_algo == AI_MINIMAX_AB) {
+        return 0; // Implement your AI here
+    } else if (ai_algo == AI_MONTE_CARLO) {
+        return 0; // Implement your AI here
+    }
+    return 0;
 }
 
 #endif // AI_MOVE_H
